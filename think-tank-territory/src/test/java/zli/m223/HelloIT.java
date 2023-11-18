d6015d397c815d64e5d82654bc86bc595a9a7758
sodaba.hamid <sodaba.hamid@edu.tbz.ch>
2023-11-18 16:10:26 +0100

    initialize new project think-tank-territory

---
+++ think-tank-territory/src/test/java/zli/m223/HelloIT.java
@@ -0,0 +1,8 @@
+package zli.m223;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloIT extends HelloTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ think-tank-territory/src/test/java/zli/m223/HelloTest.java
@@ -0,0 +1,21 @@
+package zli.m223;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class HelloTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hallo")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from RESTEasy Reactive"));
+    }
+
+}
